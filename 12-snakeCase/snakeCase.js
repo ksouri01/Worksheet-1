63c526029bf3ccd77042698360ab293ead2faa8c
kevinsour <kevinsour@nandos-ltmb-kev.local>
2021-10-18 05:15:02 +0100

    add snakeCase.js solution

--- 12-snakeCase/snakeCase.js
+++ 12-snakeCase/snakeCase.js
@@ -1,5 +1,16 @@
-const snakeCase = function() {
-
+const snakeCase = function (string) {
+    if (string.includes("-")) {
+        string = string.replaceAll("-", " ")
+    }
+    if (string.includes("..") && !string.includes("....")) {
+        string = string.replaceAll("..", " ").toLowerCase();
+    }
+    return string.replace(/\d+/g, ' ')
+        .split(/ |\B(?=[A-Z])/)
+        .map((word) => word.toLowerCase())
+        .join('_')
+        .replace(/[.,\/#!$%\^&\*;:{}=\-`~()?]/g, '')
+        .replace(/\s/g, '').toLowerCase()
 }
 
 module.exports = snakeCase
