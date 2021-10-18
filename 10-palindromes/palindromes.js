c5e72f8abdefd35c1cf4f5810048a4bb75445b42
kevinsour <kevinsour@nandos-ltmb-kev.local>
2021-10-18 04:58:17 +0100

    add palindrome and pigLatin solution

--- 10-palindromes/palindromes.js
+++ 10-palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
+const palindromes = function (string) {
 
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').replace(/\s/g, '').toLowerCase();
+    reverseString = string.split('').reverse().join('');
+    return string === reverseString;
 }
 
 module.exports = palindromes
--- 11-pig_latin/pigLatin.js
+++ 11-pig_latin/pigLatin.js
@@ -1,9 +1,31 @@
-function translate() {
-	// body...
+function translate(string) {
+	string = string.toLowerCase();
+	let words = string.split(" ");
+
+	let returnPigLatin = words.map(word => {
+		let n = word.match(/[aeiou]/gi).length;
+		switch (n) {
+			case 0: word = word + "way"; break;
+			case -1: word = word + "ay"; break;
+			default:
+				if (word.includes("qu")) {
+					word = word.replace(/([^aeio]*)([aeio])(\w+)/, "$2$3$1ay");
+				} else if (n === 1 && word.length === 3 && word.slice(word.length - 1).match(/[aeiou]/gi)) {
+					word = word.replace(/([^aeiou]*)(\w+)/, "$2$1ay");
+				}
+				else {
+					word = word.replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
+				}
+				break;
+		}
+		return word;
+	})
+
+	return returnPigLatin.join(" ");
+
 }
 
 
 module.exports = {
 	translate
 }
-
