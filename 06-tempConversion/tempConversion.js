7db31e9f7a58abf460e6df4db5cc1c0747aa532a
kevinsour <kevinsour@nandos-ltmb-kev.local>
2021-10-18 02:12:49 +0100

    add tempConversion solution

--- 06-tempConversion/tempConversion.js
+++ 06-tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function (temperature) {
+  return Math.round(((temperature - 32) / 1.8) * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function (temperature) {
+  return Math.round(((temperature * 1.8) + 32) * 10) / 10;
 }
 
 module.exports = {
