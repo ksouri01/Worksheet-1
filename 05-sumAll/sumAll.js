0eabf1c61b4bf8cbaac67c5586f3d6cf5aa2140c
kevinsour <kevinsour@nandos-ltmb-kev.local>
2021-10-18 01:54:34 +0100

    add sumAll solution

--- 05-sumAll/sumAll.js
+++ 05-sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+    let range = 0;
+    let start;
+    let end;
+
+    if (!Number.isFinite(firstNumber) || !Number.isFinite(secondNumber) || firstNumber < 0 || secondNumber < 0) {
+        return 'ERROR';
+    }
+
+    if (secondNumber < firstNumber) {
+        start = secondNumber;
+        end = firstNumber;
+    }
+    else {
+        start = firstNumber;
+        end = secondNumber;
+    }
+
+    range = (end) - start;
+    range++;
+
+    return range * (range + 1) / 2;
 
 }
 
