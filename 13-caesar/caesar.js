7ac8cd11a7f5f0a5a1ce2f2ebf03a58b186a7252
kevinsour <kevinsour@nandos-ltmb-kev.local>
2021-10-18 06:20:42 +0100

    add caesar.js solution

--- 13-caesar/caesar.js
+++ 13-caesar/caesar.js
@@ -1,5 +1,37 @@
-const caesar = function() {
+const caesar = function (sentence, number) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+
+    if (number < 0) {
+        number = number * -1;
+        if (number >= 26) {
+            number = (number % 26);
+            number = number * -1;
+        } else {
+            number = number * -1;
+        }
+    }
+
+    return cypheredSentence = sentence.split("").map(letter => {
+        let currentLetterPosition = alphabet.indexOf(letter);
+
+        if (letter === letter.toUpperCase()) {
+            currentLetterPosition = alphabet.indexOf(letter.toLowerCase());
+        }
+        let shiftedPosition = currentLetterPosition + number;
+        if (shiftedPosition >= 26) {
+            shiftedPosition = (shiftedPosition % 26);
+        }
+
+        if (!alphabet.includes(letter.toLowerCase())) {
+            return letter;
+        }
+        if (letter === letter.toUpperCase()) {
+            return alphabet.charAt(shiftedPosition).toUpperCase()
+        } else if (letter === letter.toLowerCase()) {
+            return alphabet.charAt(shiftedPosition).toLowerCase()
+        }
+    }).join('')
 }
 
 module.exports = caesar
